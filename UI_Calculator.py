94982ab5b1a5091081e47c833fd669b292aeff36
saltedcake <92123556+saltedcake@users.noreply.github.com>
2022-03-24 19:05:18 -0400

    just changing button dimensions so they all look the same

--- UI_Calculator.py
+++ UI_Calculator.py
@@ -115,13 +115,13 @@ Button(calculator, text=" . ", font=(BOLD, 16), bg="#EB8A90", activebackground="
 Button(calculator, text=" / ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=2,
        width=12, command=lambda: press("/")).grid(row=5, column=0)
 
-Button(calculator, text=" = ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=3,
+Button(calculator, text=" = ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=2,
        width=12, command=equal).grid(row=6, column=0, )
 
-Button(calculator, text=" BACKSPACE ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=3,
+Button(calculator, text=" BACKSPACE ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=2,
        width=12, command=backspace).grid(row=6, column=1, )
 
-Button(calculator, text=" CLEAR ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=3,
+Button(calculator, text=" CLEAR ", font=(BOLD, 16), bg="#EB8A90", activebackground="#d66b74", fg="black", height=2,
        width=12, command=clear).grid(row=6, column=2)
 
 calculator.bind("7", lambda x: press(7))
